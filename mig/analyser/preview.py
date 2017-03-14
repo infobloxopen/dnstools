88509867df1b658457ddc26fc242c02118654872
Vasili Vasilyeu <vasili.v@tut.by>
2017-03-14 13:15:05 +0300

    Added preview analyser

---
+++ mig/analyser/preview.py
@@ -0,0 +1,50 @@
+import sys
+import json
+
+import numpy
+from scipy.optimize import leastsq
+
+def jload(name):
+    with open(name) as f:
+       data = json.load(f)
+
+    sends = data.get("sends", [])
+    receives = data.get("receives", [])
+    pairs = data.get("pairs", [])
+
+    receives = filter(lambda x: x > 0, receives)
+
+    return sends, receives, pairs
+
+__NANOSECONDS = 1e9
+
+def calculate_fit(timestamps, start, params):
+    def linear_count(x, timestamp):
+        return x[0] + x[1]*(timestamp - start)/__NANOSECONDS
+
+    def linear_count_residuals(x, timestamp, count):
+        return linear_count(x, timestamp) - count
+
+    if params:
+        left, right = tuple(params[0])
+    else:
+        left = timestamps[0] - start
+        right = timestamps[-1] - start
+
+    points = [(timestamp, i + 1) for i, timestamp in enumerate(timestamps) if left <= timestamp - start <= right]
+    timestamp_train, count_train = zip(*points)
+
+    timestamp_train = numpy.array(timestamps)
+    count_train = numpy.array(range(1, len(timestamps) + 1))
+
+    x0 = params[1] if len(params) > 1 else [0., 1.]
+    return leastsq(linear_count_residuals, numpy.array(x0),
+                   args=(numpy.array(timestamp_train), numpy.array(count_train)))[0]
+
+def main():
+    sends, receives, pairs = jload(sys.argv[1])
+    print json.dumps({"rate": calculate_fit(sends, sends[0], [])[1], "lost": len(sends) - len(receives)})
+    return 0
+
+if __name__ == '__main__':
+    sys.exit(main())
